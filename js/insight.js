f090f1ec83636dda28acd2866196bb4aa8bb5383
Aswin <ltaswin@gmail.com>
2026-04-24 16:29:37 +0400

    Update logo source across multiple JavaScript files to use new branding image 'OPI-DWC.png'

--- js/insight.js
+++ js/insight.js
@@ -3,7 +3,7 @@ window.OpulentSite.init({
         homeHref: 'index.html',
         brandKey: 'brand',
         brandLabel: 'Opulent Prime',
-        logoSrc: 'assets/images/logo_black.png',
+        logoSrc: 'assets/images/OPI-DWC.png',
         logoAlt: 'Opulent Prime',
         nav: [
             {
